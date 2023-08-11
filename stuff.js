57cac48f98b76a68730be1f8f2827874f70be63e
Nolstarrzz <125843678+Nolstarrzz@users.noreply.github.com>
2023-08-11 09:52:07 -0700

    got the grid to work with different numbers
    need to add a way for user to change number of squares and
    make the color change when hovered over with mouse

--- stuff.js
+++ stuff.js
@@ -1,18 +1,23 @@
 const container = document.querySelector('#container');
-let blockSize = 16;
+let blockSize = 64;
 
-container.style.width = (blockSize * blockSize) + 'px';
+/*container.style.width = (blockSize * blockSize) + 'px';
 container.style.display = 'grid';
 container.style.gridTemplateColumns = 'repeat(' + blockSize + ', ' + blockSize + 'px)';
-container.style.gridTemplateRows = 'repeat(' + blockSize + ', ' + blockSize + 'px)';
+container.style.gridTemplateRows = 'repeat(' + blockSize + ', ' + blockSize + 'px)';*/
 
-for (let i = 0; i < 16; i++) {   
-    for (let j = 0; j < 16; j++) {
+container.style.display = 'flex';
+container.style.width = '500px';
+container.style.flexWrap = 'wrap';
+container.style.height = '500px'
+
+for (let i = 0; i < blockSize; i++) {   
+    for (let j = 0; j < blockSize; j++) {
         const blocks = document.createElement('div');
         blocks.classList.add('paintSurface');
-        blocks.style.width = blockSize + 'px';
-        blocks.style.height = blockSize + 'px';
-        blocks.textContent = 'hi';
+        //blocks.style.width = `${100/blockSize}%`;
+        //blocks.style.height = `${100/blockSize}%`;
+        blocks.style.flex = `0 0 ${100/blockSize}%`;
         container.appendChild(blocks);
     }
 }
