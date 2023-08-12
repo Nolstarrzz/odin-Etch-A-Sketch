c1da49314e2622383ceb4baa6937595fb1158ac6
Nolstarrzz <125843678+Nolstarrzz@users.noreply.github.com>
2023-08-11 18:51:46 -0700

    Made the number of divs into a function
    need to make hover mechanics

--- stuff.js
+++ stuff.js
@@ -1,23 +1,27 @@
 const container = document.querySelector('#container');
-let blockSize = 64;
 
-/*container.style.width = (blockSize * blockSize) + 'px';
-container.style.display = 'grid';
-container.style.gridTemplateColumns = 'repeat(' + blockSize + ', ' + blockSize + 'px)';
-container.style.gridTemplateRows = 'repeat(' + blockSize + ', ' + blockSize + 'px)';*/
 
 container.style.display = 'flex';
 container.style.width = '500px';
 container.style.flexWrap = 'wrap';
 container.style.height = '500px'
 
-for (let i = 0; i < blockSize; i++) {   
-    for (let j = 0; j < blockSize; j++) {
-        const blocks = document.createElement('div');
-        blocks.classList.add('paintSurface');
-        //blocks.style.width = `${100/blockSize}%`;
-        //blocks.style.height = `${100/blockSize}%`;
-        blocks.style.flex = `0 0 ${100/blockSize}%`;
-        container.appendChild(blocks);
+/*container.style.width = (blockSize * blockSize) + 'px';
+container.style.display = 'grid';
+container.style.gridTemplateColumns = 'repeat(' + blockSize + ', ' + blockSize + 'px)';
+container.style.gridTemplateRows = 'repeat(' + blockSize + ', ' + blockSize + 'px)';*/
+
+
+function makeBlocks(number)
+{    
+    for (let i = 0; i < number; i++) {   
+        for (let j = 0; j < number; j++) {
+            const blocks = document.createElement('div');
+            blocks.classList.add('paintSurface');
+            blocks.style.flex = `0 0 ${100/number}%`;
+            container.appendChild(blocks);
+        }
     }
 }
+
+makeBlocks(16)
